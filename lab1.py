cd0be4de6904cd38a0d9fde8b3690aa2454ddc0e
krijalgwachha <krijalgwachha88@gmail.com>
2024-08-28 09:01:06 +0545

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,100 @@
+""" maximum = 10
+for number in range(1, maximum+1 ):
+    
+    if number % 2 == 0:
+        print(number)  """
+ 
+# division
+""" a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+integer_division = a // b
+float_division = a / b
+print(integer_division)
+print(float_division) """
+
+
+#type
+""" result=3*1.5+4
+print(result)
+print(type(result)) """
+
+#square
+""" import math
+number = float(input("Enter a number: "))
+square = number ** 2
+square_root = math.sqrt(number)
+print(f"The square of {number} is: {square}")
+print(f"The square root of {number} is: {square_root}")
+ """
+# Define the color list
+""" color_list = ["red", "green", "white", "black", "blue"]
+first_color = color_list[0]
+last_color = color_list[-2]
+print(f"The first color is: {first_color}")
+print(f"The last color is: {last_color}") """
+
+# voume in M
+""" r=float(input("enter radius:"))
+volume = (4/3) * 3.14 * r**3
+print(f"voume of sphere:{volume:0.2f}") """
+
+
+# to calculate the sum or three times the sum
+""" def calculate_sum(a, b, c):
+    if a == b == c:
+        return 3 * (a + b + c)
+    else:
+        return a + b + c
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+result = calculate_sum(a, b, c)
+print(f"The result is: {result}") """
+
+#list tuple set
+""" list=["a",5]
+tuple=(5)
+set={5}
+ """
+""" a = int(input("Enter the first number: ")) """
+#if 5>4 and 3==5:
+""" print(10 > 9)
+print(10 == 9)
+print(10 < 9) """
+ 
+""" s=True
+print(not s)
+p=False
+print(not p) """
+
+""" print("yTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+ """
+
+""" x=int(input("x:"))
+y=(input("y:"))
+print(x,y) """
+ 
+""" print(3/7) """
+""" x=1
+y=2.3
+z=1j
+a=float(x)
+b=int("3ab")
+c=complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+ """
+""" data="my name is "
+print(data[-2]) """
+
+""" x="hello happy"
+#print(x.replace("E","W"))
+print(x.split(" ")
+      ) """
+#print(x[:-1::1])
+txt ="h-ello world"
+print("helloworld" in txt) 
